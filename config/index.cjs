919678d589dddff75ccbf71d6c26253919efbe4d
aa20021114 <142730447+aa20021114@users.noreply.github.com>
2023-08-20 21:30:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -1,39 +1,39 @@
 /* eslint-disable */
 
 /**
- * 此项目配置为方便新人使用，已缩减至最简配置。
- * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
- * 自行添加属性，以支持更多个性化功能
- */
-const USER_CONFIG = {
+* 此项目配置为方便新人使用，已缩减至最简配置。
+* 如若想使用更多功能，请查考文档中的【3. config参数说明】
+* 自行添加属性，以支持更多个性化功能
+*/
+const  USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID : 'wx6f137bf4de067f10' ,
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET : '3a4c87a13a139bff5f6139d5f1a3a607' ,
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE : '河北' ,
+  CITY : '保定' ,
 
-  USERS: [
+  USERS : [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name : '刘嘉琪' ,
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id : 'oOiGc6M5BuL5pQiNk3GEbFOBxhC8' ,
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId : 'kMUtYXtaQQ2wjd6IrJDykgKybfvifEUy2N04H4O7cBg' ,
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+      horoscopeDate : '03-26' ,
+      festivals : [
+        // 注意：此条配置日期为阴历日期，因为`type`中“生日” 之前有* 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+          type : '*生日' , name : '宝贝' , year : '2003' , date : '03-26' ,
+        } ,
+        // 注意：此条配置日期为阳历日期，因为`type`中“生日” 之前没有* 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type : '生日' , name : '李四' , year : '1996' , date : '09-31' ,
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
@@ -42,7 +42,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-07-26' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'kMUtYXtaQQ2wjd6IrJDykgKybfvifEUy2N04H4O7cBg',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oOiGc6M5BuL5pQiNk3GEbFOBxhC8',
     }
   ],
 
